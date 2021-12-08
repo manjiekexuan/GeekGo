04cd3860ba58008abc7f8cd9ec1fcffd7400706b
jiekexuan <jiekexuan_man@126.com>
2021-12-08 17:14:24 +0800

    修正更新202111071714

--- homework01/main.go
+++ homework01/main.go
@@ -9,22 +9,20 @@ import (
 func main() {
 
 	var inputStr string
-	// for 循环初始值
-	var count int = 3
-
+	var yesOrNo  string
 	var sexWeight int
-	//循环输入次数
-	fmt.Println("姓名;性别(男,女);身高(米);体重(公斤);年龄 ------> 例如:小治;男;1.81;86.5;30")
 
-	for i:=1;i<=count;i++ {
-		fmt.Printf("请输入第%v个人的信息",i)
-		fmt.Println()
+	//循环输入次数
+
+
+	for  {
+		fmt.Println("姓名;性别(男,女);身高(米);体重(公斤);年龄 ------> 例如:小治;男;1.81;86.5;30")
 		fmt.Scanln(&inputStr)
+
 		input := strings.Split(inputStr,";")
 		//判断参数个数是否正确
 		if len(input) < 5 || len(input) > 5 {
 			fmt.Println("输入的参数个数不正确请重新输入")
-			count++
 		}
 		name := input[0]
 		sex  := input[1]
@@ -36,21 +34,25 @@ func main() {
 		height, err := strconv.ParseFloat(input[2], 64)
 		if err != nil {
 			fmt.Println("输入的身高不正确请重新输入")
-			count++
+			continue
 		}
 		weight, err := strconv.ParseFloat(input[3], 64)
 		if err != nil {
 			fmt.Println("输入的体重不正确请重新输入")
-			count++
+			continue
 		}
 		age,err:= strconv.ParseInt(input[4],10,0)
 		if err != nil {
 			fmt.Println("输入的年龄不正确请重新输入")
-			count++
+			continue
 		}
 		BMI,FatRate,device:= getPersonReport(weight,height,age,sexWeight)
 		fmt.Println(name,BMI,FatRate,device)
-
+		fmt.Println("是否继续输入是/否(Y/N)")
+		fmt.Scanln(&yesOrNo)
+		if yesOrNo == "N"{
+			break
+		}
 	}
 	fmt.Println("已经输入结束,报表如下")
 }
@@ -63,12 +65,91 @@ func getPersonReport(weight float64,height float64,age int64, sexWeight int )(fl
 
 }
 
+//获取建议
 func getDevice(fatRate float64,age int64,sexWeight int )string{
-	//男性体脂
+	var content string
 	if sexWeight == 1{
-
+		content =getDeviceContentMan(fatRate,age)
+	}else if sexWeight == 2{
+		content = getDeviceContentWoman(fatRate,age)
 	}
+	return content
 }
+
+func getDeviceContentMan(fatRate float64,age int64) string {
+	var content string
+	switch  {
+		case fatRate >= 0 && fatRate <=10:
+			content = "偏瘦"
+			fallthrough
+		case fatRate >=30 && fatRate<=45:
+			content = "严重肥胖"
+			fallthrough
+		case 18 <= age && age<= 39:
+			if fatRate <= 16{
+				content = "标准"
+			}else if fatRate <= 21{
+				content = "偏重"
+			}else if fatRate >= 22 && fatRate <= 26{
+				content = "肥胖"
+			}
+		case 40 <= age && age<= 59:
+			if fatRate >= 12 && fatRate <= 17{
+				content = "标准"
+			}else if fatRate >=18 && fatRate <= 22{
+				content = "偏重"
+			}else if fatRate >=23 && fatRate<=27{
+				content = "肥胖"
+			}
+
+		case age >= 60:
+			if fatRate >= 14 && fatRate<=19{
+				content = "标准"
+			}else if fatRate >= 20 && fatRate<= 24{
+				content = "偏重"
+			}else if fatRate >= 25 && fatRate<=29 {
+				content = "肥胖"
+			}
+	}
+	return content
+}
+
+func getDeviceContentWoman(fatRate float64,age int64) string{
+	var content string
+	switch  {
+		case fatRate >= 0 && fatRate <=20:
+			content = "偏瘦"
+			fallthrough
+		case 18 <= age && age<= 39:
+			if fatRate <= 27{
+				content = "标准"
+			}else if fatRate <= 34{
+				content = "偏重"
+			}else if fatRate <= 39{
+				content = "肥胖"
+			}
+		case 40 <= age && age<= 59:
+			if fatRate <= 28{
+				content = "标准"
+			}else if fatRate <= 35{
+				content = "偏重"
+			}else if fatRate<=40{
+				content = "肥胖"
+			}
+		case age >= 60:
+			if fatRate <=22{
+				content = "偏瘦"
+			} else if fatRate <= 29{
+				content = "标准"
+			}else if fatRate <= 36{
+				content = "偏重"
+			}else if  fatRate<=41 {
+				content = "肥胖"
+			}
+	}
+	return content
+}
+
 
 func getBMI(weight float64,height float64) float64{
 	//bmi := weight / (height * height)
